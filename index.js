f6171db908ebe31e9c57f3469a5b18aa7e128005
Joanna-Lazarte <jlazartelagos@gmai.com>
2023-06-13 03:58:40 -0300

    jquery

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 //MOSTRAR ASIDE
 const btnAside = document.getElementById('btn-aside'); //fuera del aside
 const boxAside = document.querySelector('.box-aside');
@@ -34,4 +35,21 @@ servs.addEventListener('click', ()=>{
     btnAside.addEventListener('click', () => {
         boxAside.style.display = 'flex';
     });
+}); */
+//Mostrar y ocultar aside con btn-aside y icono-barras, usando jquery
+$(document).ready(function(){
+    $("#btn-aside").click(function(){
+        $(".box-aside").show();
+    });
 });
+
+$(document).ready(function(){
+    $(".icono-barras").click(function(){
+        $(".box-aside").hide();
+    });
+});
+
+//Ocultar aside al seleccionar el menu aside, con jquery
+
+
+
