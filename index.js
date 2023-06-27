f32ac387b8378d5493cbe31a52336180983add6d
Joanna-Lazarte <jlazartelagos@gmai.com>
2023-06-27 03:17:08 -0300

    aside-jq

--- index.js
+++ index.js
@@ -1,41 +1,4 @@
-/*
-//MOSTRAR ASIDE
-const btnAside = document.getElementById('btn-aside'); //fuera del aside
-const boxAside = document.querySelector('.box-aside');
-const iconoBarras = document.querySelector('.icono-barras'); //dentro del aside
 
-btnAside.addEventListener('click', () => {
-    boxAside.style.display = 'flex';
-    iconoBarras.addEventListener('click', () => {
-        boxAside.style.display = 'none';
-    });
-});
-
-
-//BOTON ASIDE 
-iconoBarras.addEventListener('click', () => {
-    boxAside.style.display = 'none';
-    btnAside.addEventListener('click', () => {
-        boxAside.style.display = 'flex';
-    });
-});
-
-//
-const nos = document.getElementById('nos');
-const servs =document.getElementById('servs');
-nos.addEventListener('click', ()=>{
-    boxAside.style.display = 'none';
-    btnAside.addEventListener('click', () => {
-        boxAside.style.display = 'flex';
-    });
-});
-
-servs.addEventListener('click', ()=>{
-    boxAside.style.display = 'none';
-    btnAside.addEventListener('click', () => {
-        boxAside.style.display = 'flex';
-    });
-}); */
 //Mostrar y ocultar aside con btn-aside y icono-barras, usando jquery
 $(document).ready(function(){
     $("#btn-aside").click(function(){
@@ -50,6 +13,28 @@ $(document).ready(function(){
 });
 
 //Ocultar aside al seleccionar el menu aside, con jquery
+$(document).ready(function(){
+           
+    $(".menu-solapa").click(function(){
+        $(".box-aside").hide();
+       
+    });
+});
 
+$(document).ready(function(){
+           
+    $(".opS").click(function(){
+        $(".box-aside").hide();
+       
+    });
+});
+
+$(document).ready(function(){
+           
+    $(".sp-btn").click(function(){
+        $(".box-aside").hide();
+       
+    });
+});
 
 
